21927ce8aab78eb9db9ce62a97121847f5333b08
emanuelenardi <nardiemanuele6@gmail.com>
2021-02-23 00:47:55 +0100

    indice commentato

--- src/scripts/windows/compile-preambles.bat
+++ src/scripts/windows/compile-preambles.bat
@@ -5,12 +5,12 @@ set INITIAL_WORKING_DIRECTORY=%cd%
 
 echo: & echo ==================== COMPILO PREAMBOLO PRINCIPALE ==================== & echo:
 cd "%SETTINGS_DIR%"
-pdflatex --interaction=batchmode --shell-escape -ini -jobname="preamble.main" "&pdflatex ./preamble.main.sty\dump"
+pdflatex --shell-escape -ini -jobname="preamble.main" "&pdflatex ./preamble.main.sty\dump"
 del /s preamble.main.log >nul 2>&1
 
 echo: & echo ==================== COMPILO PREAMBOLO ALGORITMI ==================== & echo:
 cd "%ALGORITHMS_DIR%"
-pdflatex --interaction=batchmode -ini -jobname="preamble" "&pdflatex preamble.sty\dump"
+pdflatex -ini -jobname="preamble" "&pdflatex preamble.sty\dump"
 del /s preamble.log >nul 2>&1
 
 cd %INITIAL_WORKING_DIRECTORY%
