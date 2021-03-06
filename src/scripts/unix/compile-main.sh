bb1aa20ee9e153780772a1e1ad53a329ca1d8850
emanuelenardi <nardiemanuele6@gmail.com>
2021-03-06 16:26:37 +0100

    rimozione indice dagli script e semplificazione

--- src/scripts/unix/compile-main.sh
+++ src/scripts/unix/compile-main.sh
@@ -6,11 +6,6 @@ INITIAL_WORKING_DIRECTORY=$(pwd)
 MESSAGE="\n==================== COMPILO DOCUMENTO PRINCIPALE ====================" ; simple_blue_echo
 cd $SRC_DIR
 arara main
-# pdflatex --shell-escape --output-directory=build main.tex
-# cd $BUILD_DIR
-# makeindex -s ../settings/index-style.ist main
-# cd $SRC_DIR
-# pdflatex - q-shell-escape --output-directory=build main.tex
 
 cd $BUILD_DIR
 find . -type f ! -name '*.pdf' ! -name '.gitignore' | xargs -r rm
--- src/scripts/windows/compile-main.bat
+++ src/scripts/windows/compile-main.bat
@@ -6,28 +6,12 @@ set INITIAL_WORKING_DIRECTORY=%cd%
 
 echo: & echo ==================== COMPILO DOCUMENTO PRINCIPALE ==================== & echo:
 cd %SRC_DIR%
-arara -v main
-@REM pdflatex --shell-escape --output-directory=build main.tex
-@REM cd %BUILD_DIR%
-@REM makeindex -s ../settings/index-style.ist main
-@REM cd %SRC_DIR%
-@REM pdflatex --shell-escape --output-directory=build main.tex
+@REM arara -v main
 
-@REM cd %BUILD_DIR%
-@REM echo: & echo ===================== EFFETTUO PULIZIA FILE AUX ===================== & echo:
-@REM echo [ .aux, .idx, .ilg, .ind, .loa, .loe, .lof, .lol, .lot, .synctex(busy), .synctex.gz, .toc ]
-@REM rmdir /q /s _minted-cache >nul 2>&1
-@REM del /s *.aux >nul 2>&1
-@REM del /s *.idx >nul 2>&1
-@REM del /s *.ilg >nul 2>&1
-@REM del /s *.ind >nul 2>&1
-@REM del /s *.loa >nul 2>&1
-@REM del /s *.loe >nul 2>&1
-@REM del /s *.lof >nul 2>&1
-@REM del /s *.lol >nul 2>&1
-@REM del /s *.lot >nul 2>&1
-@REM del /s *.synctex(busy) >nul 2>&1
-@REM del /s *.synctex.gz >nul 2>&1
-@REM del /s *.toc >nul 2>&1
+cd %BUILD_DIR%
+echo: & echo ===================== EFFETTUO PULIZIA FILE AUX ===================== & echo:
+rmdir /q /s _minted-cache >nul 2>&1
+@REM Delete all files EXCEPT a given extension
+for /f %%F in ('dir /b /a-d ^| findstr /vile ".gitignore .pdf"') do del "%%F"
 
 cd %INITIAL_WORKING_DIRECTORY%
