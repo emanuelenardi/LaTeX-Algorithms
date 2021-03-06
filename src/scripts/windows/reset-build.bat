e1b33101b826a31ec71b727132e66fce3d111f77
emanuelenardi <nardiemanuele6@gmail.com>
2021-03-06 16:27:00 +0100

    script di pulizia build windows più completo

--- src/scripts/windows/reset-build.bat
+++ src/scripts/windows/reset-build.bat
@@ -3,7 +3,7 @@ call "%~dp0\common.bat"
 
 set INITIAL_WORKING_DIRECTORY=%cd%
 
-echo ================= CANCELLO CONTENUTO CARTELLA BUILD ==================
+echo: & echo ===================== CANCELLO CONTENUTO CARTELLA BUILD ===================== & echo:
 cd %BUILD_DIR%
 
 @REM cache algoritmi
@@ -17,11 +17,16 @@ del /s *.pdf >nul 2>&1
 @REM file intermedi
 del /s *.aux >nul 2>&1
 del /s *.idx >nul 2>&1
+del /s *.ilg >nul 2>&1
+del /s *.ind >nul 2>&1
 del /s *.loa >nul 2>&1
-del /s *.loc >nul 2>&1
+del /s *.loe >nul 2>&1
 del /s *.lof >nul 2>&1
 del /s *.log >nul 2>&1
+del /s *.lol >nul 2>&1
 del /s *.lot >nul 2>&1
+del /s *.pyg >nul 2>&1
+del /s *.synctex.gz >nul 2>&1
 del /s *.synctex(busy) >nul 2>&1
 del /s *.toc >nul 2>&1
 
