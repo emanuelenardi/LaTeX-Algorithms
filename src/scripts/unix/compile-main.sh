d940b7bb53c06576c9efd278fcffa1af3ceffde8
emanuelenardi <nardiemanuele6@gmail.com>
2021-03-06 15:34:38 +0100

    sistemato preambolo singoli capitoli

--- src/scripts/unix/compile-main.sh
+++ src/scripts/unix/compile-main.sh
@@ -5,12 +5,12 @@ INITIAL_WORKING_DIRECTORY=$(pwd)
 
 MESSAGE="\n==================== COMPILO DOCUMENTO PRINCIPALE ====================" ; simple_blue_echo
 cd $SRC_DIR
-arara -v main
+arara main
 # pdflatex --shell-escape --output-directory=build main.tex
 # cd $BUILD_DIR
 # makeindex -s ../settings/index-style.ist main
 # cd $SRC_DIR
-# pdflatex --shell-escape --output-directory=build main.tex
+# pdflatex - q-shell-escape --output-directory=build main.tex
 
 cd $BUILD_DIR
 find . -type f ! -name '*.pdf' ! -name '.gitignore' | xargs -r rm
