e0c958dbae3d3cccc98f89678e06e778014ca9e8
emanuelenardi <nardiemanuele6@gmail.com>
2021-03-16 02:33:13 +0100

    🐛 FIX: path main

--- src/scripts/unix/check-files.sh
+++ src/scripts/unix/check-files.sh
@@ -6,7 +6,7 @@ chapters_pdf=$(ls -A $BUILD_DIR/chapters | wc -l)
 
 echo "number of chapters:     $chapters_tex"
 echo "number of pdf compiled: $chapters_pdf"
-[[ -f main.pdf ]] && echo '"main.pdf" exist' || echo '"main.pdf" does not exist'
+[[ -f $BUILD_DIR/main.pdf ]] && echo '"main.pdf" exist' || echo '"main.pdf" does not exist'
 
 isCompilationRanCorrectly() {
   if [ $chapters_pdf -eq $chapters_tex ] && [ -f $BUILD_DIR/main.pdf ]; then
